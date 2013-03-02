adeb1459b5667900c132c51975563206f8bfb318
Nigel N. <nigel.c1981@gmail.com>
2013-03-01 21:14:42 -0800

    Update application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  $(".post:submit").click(function(e){
+  $(".post:submit").click(function(e){ // AA: instead of being so specific, can pull the form
     e.preventDefault();
     $.ajax({
       type:"post", //can be get or post
       url:"/posts/vote", //go to this url
-      data: {post_id: $(this).siblings().val()}, //data sent to the controller
-      dataType: "json" //expects to receive json data as response from controller
+      data: {post_id: $(this).siblings().val()}, //data sent to the controller/server //AA: if pull form, can do serialize data instead of having to do this
+      dataType: "json" //sending data in json format to the controller/server
     }).success(function(response) { 
       console.log(response);
       var post_id = response.post_id;
@@ -19,3 +19,9 @@ $(document).ready(function(){
 });
 
 //do commentvotes next
+
+//reason why error doesn't work is because the controller is not throwing up an error that ajax recognizes. If you want
+//to use save to trigger the error, have to use save! but we don't want to do that because it stops everything.
+//so in the controller put an if else statement just send the data that we want to show depending on whether it is
+//a success or an error according to how we expect it to be and just make those messages appear in the success
+//function in ajax
